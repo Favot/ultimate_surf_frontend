f696f3e142cd9f23d6c3b0d733c54f6d873da22c
Justine Saupin <sau.justine@gmail.com>
2021-06-04 13:27:17 +0200

    attention ca marche pô

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,6 +1,8 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
+export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_live_A42B5B43C7706DDA'
+
 export const fromImageToUrl = (image) => {
   if (!image) {
     return "/vercel.svg"; //Or default image here
